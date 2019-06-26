b4ea45046ed2519049f57b7eefbf1759572d14bf
Gavin Farrell <gavin-farrell@hotmail.com>
2019-06-26 02:34:41 -0700

    Updated info message and calculations

--- src/io/github/Arkrimus/MinionCalculator.java
+++ src/io/github/Arkrimus/MinionCalculator.java
@@ -8,39 +8,39 @@ import java.awt.event.*;
 import java.util.ArrayList;
 
 public class MinionCalculator extends JFrame implements ActionListener{
+    //Minion types, load in the time per level of each minion.
     enum MinionType {
-        WHEAT(15,15,13,13,11,11,10,10,9,9,8),
-        CARROT(20,20,18,18,16,16,14,14,12,12,10),
+        WHEAT(15,15,13,13,11,11,10,10,9,9,8),           // Wheat minion assumes you are only trying to get wheat not seeds.
+        CARROT(20,20,18,18,16,16,14,14,12,12,10),       // Carrot/potato minion assume that you get an average of 2.5 crops per harvest.
         POTATO(20,20,18,18,14,14,12,12,10,10),
         PUMPKIN(40,40,37,37,34,34,30,30,25,25,20),
-        MELON(30,30,28,28, 26,26,23,23,20,20,16),
+        MELON(30,30,28,28, 26,26,23,23,20,20,16),       // Melon minion assumes that you get 5.5 melons per harvest.
         MUSHROOM(30,30,28,28,26,26,23,23,20,20,16),
-        COCOA_BEAN(27,27,25,25,23,23,21,21,18,18,15),
-        CACTUS(27,27,25,25,23,23,21,21,18,18,15),
+        COCOA_BEAN(27,27,25,25,23,23,21,21,18,18,15),   // Cocoa beans assume you get 3 every harvest.
+        CACTUS(27,27,25,25,23,23,21,21,18,18,15),       // Cactus/Sugar Cane minion assumes the cactus is fully grown.
         SUGAR_CANE(27,27,25,25,23,23,21,21,18,18,15),
-        COW(26,26,24,24,22,22, 20,20,17,17,13),
+        COW(26,26,24,24,22,22, 20,20,17,17,13),         // Cow minion assumes you are only trying to get leather.
         PIG(26,26,24,24,22,22,20,20,17,17,13),
-        CHICKEN(26,26,24,24,22,22,20,20,17,17,13),
-        SHEEP(24,24,22,22,20,20,18,18,16,16,12),
-        RABBIT(26,26,24,24,22,22,20,20,17,17,13),
-        NETHER_WART(50,50,47,47, 44,44,41,41, 38,38,32),
+        CHICKEN(26,26,24,24,22,22,20,20,17,17,13),      // Chicken minion assumes you are only trying to get feathers.
+        SHEEP(24,24,22,22,20,20,18,18,16,16,12),        // Sheep minion assumes you are only trying to get mutton.
+        RABBIT(26,26,24,24,22,22,20,20,17,17,13),       // Rabbit minion assumes you are only trying to get rabbit meat.
+        NETHER_WART(50,50,47,47, 44,44,41,41, 38,38,32),// Nether wart minion assumes you get an average of 3 every harvest.
         COBBLESTONE(14,14,12,12,10,10,9,9,8,8,7),
         COAL(15,15,13,13,12,12,10,10,9,9,7),
         IRON(17,17,15,15,14,14,12,12,10,10,8),
         GOLD(22,22,20,20,18,18,16,16,14,14,11),
         DIAMOND(29,29,27,27,25,25,22,22,19,19,15),
-        LAPIS_LAZULI(29,29,27,27,25,25,23,23,21,21,18),
+        LAPIS_LAZULI(29,29,27,27,25,25,23,23,21,21,18), // Lapis minion assumes you get an average of 6.5 every ore.
         EMERALD(28,28,26,26,24,24,21,21,18,18,14),
-        REDSTONE(29,29,27,27,25,25,23,23,21,21,18),
-        QUARTZ(25,25,23,23,21,21,19,19,16,16,13),
+        REDSTONE(29,29,27,27,25,25,23,23,21,21,18),     // Redstone minion assumes you get an average of 4.5 every ore.
+        QUARTZ(25,25,23,23,21,21,19,19,16,16,13),       // Quartz minion assumes you get an average of 4 quartz every ore.
         OBSIDIAN(45,45,42,42,39,39,35,35,30,30,24),
         GLOWSTONE(25,25,23,23,21,21,19,19,16,16,13),
-        GRAVEL(24,26,26,24,24,22,22,19,19,16,16,13),
+        GRAVEL(24,26,26,24,24,22,22,19,19,16,16,13),    // Gravel assumes you get only gravel with every harvest.
         SAND(26,26,24,24,22,22,19,19,16,16,13),
         ZOMBIE(26,26,24,24,22,22,20,20,17,17,13),
         SKELETON(26,26,24,24,22,22,20,20,17,17,13),
-        SPIDER(26,26,24,24,22,22,20,20,17,17,13),
-        CREEPER(27,27,25,25,23,23,21,21,18,18,14),
+        SPIDER(26,26,24,24,22,22,20,20,17,17,13),       // Spiders don't account for spider eyes, and cave spider minions are not included in this program.
         ENDERMAN(32,32,30,30,28,28,25,25,22,22,18),
         SLIME(26,26,24,24,22,22,19,19,16,16,12),
         BLAZE(30,30,28,28,26,26,23,23,19,19,15),
@@ -52,23 +52,30 @@ public class MinionCalculator extends JFrame implements ActionListener{
 
         MinionType(int... timePerLevel){
             this.timePerAction = timePerLevel;
-
         }
 
         int getTimeAtLevel(int level){return timePerAction[level];}
     }
 
+    //Items that we need to access later for calculations
     private JComboBox minions;
     private ArrayList<JSpinner> minionLevels = new ArrayList<>();
     private JTextField desiredAmt;
     private JButton calcButton = new JButton("Calculate"), resetButton = new JButton("Reset");
 
+
     private MinionCalculator(){
         super("Hypixel Minion Calculator");
+
+        //Add action listeners to the buttons
         calcButton.addActionListener(this);
         resetButton.addActionListener(this);
+
+        //Don't allow resizing because I don't know how to make it look good on bigger sizes lol
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add the first combo box with all the minion types in it.
         JPanel temp = new JPanel();
         temp.setLayout(new FlowLayout());
         temp.add(new JLabel("Minion Type: "));
@@ -78,13 +85,13 @@ public class MinionCalculator extends JFrame implements ActionListener{
                 "Sugar Cane", "Cow", "Pig", "Chicken", "Sheep", "Rabbit", "Nether Wart", "Cobblestone", "Coal", "Iron", "Gold", "Diamond",
                 "Lapis Lazuli", "Emerald", "Redstone", "Quartz", "Obsidian", "Glowstone", "Gravel", "Sand", "Zombie", "Skeleton", "Spider",
                 "Creeper", "Enderman", "Slime", "Blaze", "Magma Cube", "Clay", "Oak", "Spruce", "Birch", "Dark Oak", "Acacia", "Jungle"};
-
         minions =  new JComboBox(minionTypes);
         minions.setSelectedIndex(0);
         minions.setEditable(false);
         temp.add(minions);
         add(temp);
 
+        //Use spring layout for the level spinners. SpringUtilities is an Oracle provided utility class.
         temp = new JPanel();
         temp.setLayout(new SpringLayout());
         for (int i = 1; i <= 11; i++) {
@@ -95,6 +102,7 @@ public class MinionCalculator extends JFrame implements ActionListener{
         SpringUtilities.makeCompactGrid(temp, 11,2,0,15,3,3);
         add(temp);
 
+        //Last field for desired number of items.
         temp = new JPanel();
         temp.setLayout(new FlowLayout());
         temp.add(new JLabel("Desired Amount of items: "));
@@ -124,7 +132,9 @@ public class MinionCalculator extends JFrame implements ActionListener{
     }
 
     private void calculate(){
-        MinionType type = MinionType.valueOf(((String) minions.getSelectedItem()).replace(" ", "_").toUpperCase());
+        MinionType type;
+        if (minions.getSelectedIndex() >= 37) type = MinionType.TREE;
+        else type = MinionType.valueOf(((String) minions.getSelectedItem()).replace(" ", "_").toUpperCase());
         int totalSeconds, totalMins, totalHrs, totalDays;
         double desiredAmount;
 
@@ -132,21 +142,33 @@ public class MinionCalculator extends JFrame implements ActionListener{
         for (int i = 0; i < 11; i++){
             if ((int) minionLevels.get(i).getValue() < 1) continue;
             switch (type){
+                case LAPIS_LAZULI:{
+                    itemsPerSecond += (((int) minionLevels.get(i).getValue()*6.5) / (double) (type.getTimeAtLevel(i)));
+                    break;
+                }
+                case MELON:{
+                    itemsPerSecond += (((int) minionLevels.get(i).getValue()*5.5) / (double) (type.getTimeAtLevel(i)));
+                    break;
+                }
+                case REDSTONE:{
+                    itemsPerSecond += (((int) minionLevels.get(i).getValue()*4.5) / (double) (type.getTimeAtLevel(i)));
+                    break;
+                }
+                case QUARTZ:
                 case GLOWSTONE:{
                     itemsPerSecond += (((int) minionLevels.get(i).getValue()*4) / (double) (type.getTimeAtLevel(i)*2));
                     break;
                 }
-                case POTATO:
-                case CARROT:{
-                    itemsPerSecond += (((int) minionLevels.get(i).getValue()*3) / (double) (type.getTimeAtLevel(i)*2));
-                    break;
-                }
-                case MELON:{
-                    itemsPerSecond += (((int) minionLevels.get(i).getValue()*4) / (double) (type.getTimeAtLevel(i)));
-                }
+                case NETHER_WART:
+                case COCOA_BEAN:
                 case SUGAR_CANE:
                 case CACTUS:{
                     itemsPerSecond += (((int) minionLevels.get(i).getValue()*3) / (double) (type.getTimeAtLevel(i)));
+                    break;
+                }
+                case POTATO:
+                case CARROT:{
+                    itemsPerSecond += (((int) minionLevels.get(i).getValue()*2.5) / (double) (type.getTimeAtLevel(i)*2));
                     break;
                 }
                 default:{
@@ -155,25 +177,30 @@ public class MinionCalculator extends JFrame implements ActionListener{
                 }
             }
         }
-        System.out.println("You get " + itemsPerSecond + " items per second");
+
         try{
             desiredAmount = Integer.parseInt(desiredAmt.getText().trim());
         } catch(NumberFormatException nfe){
-            nfe.printStackTrace();
             return;
         }
-        int time = (int) (desiredAmount/itemsPerSecond);
-        System.out.println("It'll take " + time + " seconds.");
-        totalSeconds = time%60;
-        time -= time%60;
-        totalMins = (time/60)%60;
-        time -= totalMins*60;
-        totalHrs = ((time/60)/60)%24;
-        time -= totalHrs*60*60;
-        totalDays = (((time/60)/60)/24);
-        JOptionPane.showMessageDialog(null, "It will take about " + totalDays + " days, " + totalHrs + " hours, " + totalMins + " minutes, and " + totalSeconds + " seconds to reach " + desiredAmount + " to achieve your goal.");
+
+        int time = (int) (desiredAmount/itemsPerSecond);//The total time in seconds.
+
+        //Conversion process into an understandable time format.
+
+        totalSeconds = time%60;                         //Time % 60 gets us any leftover seconds
+        time -= time%60;                                //Subtract the left over seconds and you are left with a number divisible by 60
+        totalMins = (time/60)%60;                       //Divide the time by 60 and mod it by 60 to get the leftover minutes.
+        time -= totalMins*60;                           //Convert the leftover minutes into seconds and subtract it from the original time
+        totalHrs = ((time/60)/60)%24;                   //Divide time by 60 twice and mod by 24 to get the leftover hours
+        time -= totalHrs*60*60;                         //Convert the leftover hours into seconds subtract that amount from time.
+        totalDays = (((time/60)/60)/24);                //Divide by 60, 60, and 24 to convert the remaining time into days.
+
+        //Show a message dialog with the time info.
+        JOptionPane.showMessageDialog(null, "It will take about " + totalDays + " days, " + totalHrs + " hours, " + totalMins + " minutes, and " + totalSeconds + " seconds to achieve your goal.");
     }
 
+    //Reset the tool to original values.
     private void reset(){
         for (JSpinner s : minionLevels){
             s.setValue(0);
